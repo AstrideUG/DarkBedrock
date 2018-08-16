2862e8ed9e76100c2f450925b2c2be450ba16c36
LartyHD <LartyHD@minewar.de>
2018-08-16 16:02:00 +0200

    Update PingTrackerModule and fix Injector

--- DarkFrame/ClassModules/src/main/kotlin/bungee/PingTrackerModule.kt
+++ DarkFrame/ClassModules/src/main/kotlin/bungee/PingTrackerModule.kt
@@ -1,10 +1,18 @@
+import com.google.gson.JsonArray
 import net.darkdevelopers.darkbedrock.darkframe.bungee.DarkFrame
-import net.darkdevelopers.darkbedrock.darkness.bungee.listener.Listener
-import net.darkdevelopers.darkbedrock.darkness.bungee.messages.Colors.IMPORTANT
-import net.darkdevelopers.darkbedrock.darkness.bungee.messages.Colors.TEXT
+import net.darkdevelopers.darkbedrock.darkness.bungee.commands.Command
+import net.darkdevelopers.darkbedrock.darkness.bungee.messages.Colors
+import net.darkdevelopers.darkbedrock.darkness.bungee.messages.Messages
+import net.darkdevelopers.darkbedrock.darkness.general.configs.ConfigData
+import net.darkdevelopers.darkbedrock.darkness.general.configs.gson.GsonConfig
 import net.darkdevelopers.darkbedrock.darkness.general.modules.Module
 import net.darkdevelopers.darkbedrock.darkness.general.modules.ModuleDescription
+import net.md_5.bungee.api.ChatColor
+import net.md_5.bungee.api.CommandSender
+import net.md_5.bungee.api.ProxyServer
+import net.md_5.bungee.api.chat.TextComponent
 import net.md_5.bungee.api.event.ProxyPingEvent
+import net.md_5.bungee.api.plugin.Listener
 import net.md_5.bungee.event.EventHandler
 import java.util.*
 import java.util.concurrent.TimeUnit
@@ -15,13 +23,15 @@ import kotlin.concurrent.thread
  * Created by Lars Artmann | LartyHD on 05.07.2018 13:55.
  * Last edit 15.08.2018
  */
-class PingTrackerModule : Module, Listener(DarkFrame.instance) {
+class PingTrackerModule : Module, Listener, Command("PingTracker") {
 
-    override val description: ModuleDescription = ModuleDescription("PingTrackerModule", "1.0.1", "Lars Artmann | LartyHD", "This module tracks the ping requests")
+    override val description: ModuleDescription = ModuleDescription("PingTrackerModule", "1.1", "Lars Artmann | LartyHD", "This module tracks the ping requests")
 
-    private val minutePings: MutableSet<String> = HashSet()
-    private val hourPings: MutableSet<String> = HashSet()
-    private val dayPings: MutableSet<String> = HashSet()
+    private val minutePings = mutableSetOf<String>()
+    private val hourPings = mutableSetOf<String>()
+    private val dayPings = mutableSetOf<String>()
+    private val players = mutableSetOf<UUID>()
+    private lateinit var config: GsonConfig
 
     init {
         logPing(TimeUnit.MINUTES.toMillis(1), "der letzten Minute", minutePings)
@@ -29,9 +39,31 @@ class PingTrackerModule : Module, Listener(DarkFrame.instance) {
         logPing(TimeUnit.DAYS.toMillis(1), "der letzten 24 Stunden", dayPings)
     }
 
+    override fun start() {
+        config = GsonConfig(ConfigData(description.folder))
+        config.getAs<JsonArray>("players")?.forEach { players.add(UUID.fromString(it.asString)) }
+        ProxyServer.getInstance().pluginManager.registerListener(DarkFrame.instance, this)
+    }
+
+    override fun stop() {
+        config.put("players", players.toTypedArray())
+        config.save()
+    }
+
+    override fun perform(sender: CommandSender, args: Array<String>) = isPlayer(sender) {
+        val uniqueId = it.uniqueId ?: throw NullPointerException("uniqueId can not be null")
+        if (players.contains(uniqueId)) {
+            players.remove(uniqueId)
+            sender.sendMessage(TextComponent("${Messages.PREFIX}${ChatColor.GRAY}Du bekommst keine weiteren Ping logs"))
+        } else {
+            players.add(uniqueId)
+            sender.sendMessage(TextComponent("${Messages.PREFIX}${ChatColor.GRAY}Du bekommst absofort Ping logs"))
+        }
+    }
+
     @EventHandler
     fun onProxyPingEvent(event: ProxyPingEvent) {
-        val ip = event.connection.address.hostString ?: return
+        val ip = event.connection?.address?.hostString ?: return
         minutePings.add(ip)
         hourPings.add(ip)
         dayPings.add(ip)
@@ -41,11 +73,13 @@ class PingTrackerModule : Module, Listener(DarkFrame.instance) {
         try {
             while (true) {
                 Thread.sleep(sleep)
-                println("In $time ${when (pings.size) {
-                    0 -> "wurden $IMPORTANT keine ${TEXT}Ping's"
-                    1 -> "wurde $IMPORTANT ein ${TEXT}Ping"
-                    else -> "wurden $IMPORTANT ${pings.size} ${TEXT}Ping's"
-                }} dokumentiert")
+                val message = "In $time ${when (pings.size) {
+                    0 -> "wurden ${Colors.IMPORTANT} keine ${Colors.TEXT}Ping's"
+                    1 -> "wurde ${Colors.IMPORTANT} ein ${Colors.TEXT}Ping"
+                    else -> "wurden ${Colors.IMPORTANT} ${pings.size} ${Colors.TEXT}Ping's"
+                }} dokumentiert"
+                println(message)
+                players.forEach { ProxyServer.getInstance().getPlayer(it)?.sendMessage(TextComponent(message)) }
                 pings.clear()
             }
         } catch (ignored: InterruptedException) {
