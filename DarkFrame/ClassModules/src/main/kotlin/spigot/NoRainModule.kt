309c8445b8e0da81bdfb28651440a3f10eb0122c
LartyHD <LartyHD@minewar.de>
2018-08-16 16:32:32 +0200

    Update NoRainModule

--- DarkFrame/ClassModules/src/main/kotlin/spigot/NoRainModule.kt
+++ DarkFrame/ClassModules/src/main/kotlin/spigot/NoRainModule.kt
@@ -3,8 +3,10 @@ import net.darkdevelopers.darkbedrock.darkness.general.modules.Module
 import net.darkdevelopers.darkbedrock.darkness.general.modules.ModuleDescription
 import net.darkdevelopers.darkbedrock.darkness.spigot.listener.Listener
 import org.bukkit.Bukkit
+import org.bukkit.World
 import org.bukkit.event.EventHandler
 import org.bukkit.event.weather.WeatherChangeEvent
+import org.bukkit.event.world.WorldLoadEvent
 
 /**
  * @author Lars Artmann | LartyHD
@@ -12,16 +14,18 @@ import org.bukkit.event.weather.WeatherChangeEvent
  * Last edit 05.07.2018
  */
 class NoRainModule : Module, Listener(DarkFrame.instance) {
-    override val description: ModuleDescription = ModuleDescription("NoRainModule", "1.0.2", "Lars Artmann | LartyHD", "This modules block rain")
+    override val description: ModuleDescription = ModuleDescription("NoRainModule", "1.1", "Lars Artmann | LartyHD", "This modules block rain")
 
-    override fun init() {
-        for (world in Bukkit.getWorlds()) {
-            world ?: continue
-            world.setStorm(false)
-            world.isThundering = false
-        }
-    }
+    override fun init() = Bukkit.getWorlds().forEach(this::clearWeather)
 
     @EventHandler
-    fun onWeatherChangeEvent(event: WeatherChangeEvent) = cancel(event)
+    fun onWeatherChangeEvent(event: WeatherChangeEvent) = cancel(event, !event.toWeatherState())
+
+    @EventHandler
+    fun onWorldLoadEvent(event: WorldLoadEvent) = clearWeather(event.world)
+
+    private fun clearWeather(world: World) {
+        world.setStorm(false)
+        world.isThundering = false
+    }
 }
