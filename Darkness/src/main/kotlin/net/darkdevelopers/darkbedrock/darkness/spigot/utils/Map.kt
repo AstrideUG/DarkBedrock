5aa775bc5399624daf3c96057cd8aff7750e759c
LartyHD <LartyHD@minewar.de>
2018-08-18 12:07:33 +0200

    Added Map

---
+++ Darkness/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/spigot/utils/Map.kt
@@ -0,0 +1,51 @@
+/*
+ * © Copyright - Lars Artmann aka. LartyHD 2018.
+ */
+
+package net.darkdevelopers.darkbedrock.darkness.spigot.utils
+
+import net.darkdevelopers.darkbedrock.darkness.spigot.region.Region
+import org.bukkit.Location
+import org.bukkit.entity.Player
+import java.util.*
+
+
+/**
+ * @author Lars Artmann | LartyHD
+ * Created by Lars Artmann | LartyHD on 12.08.2018 20:49.
+ * Last edit 15.08.2018
+ */
+data class Map(
+        val name: String,
+        val spawn: Location,
+        val hologram: Location,
+        val region: Region,
+        val sendHolograms: (Player, MutableMap<UUID, Holograms>) -> Unit
+) {
+
+    private val holograms = mutableMapOf<UUID, Holograms>()
+
+    fun sendHologram(player: Player) {
+        removeHologram(player)
+        sendHolograms(player, holograms)
+//        KnockIT.instance.stats.get(player.uniqueId, "kills") { kills ->
+//            KnockIT.instance.stats.get(player.uniqueId, "deaths") { deaths ->
+//                holograms[player.uniqueId] = Holograms(arrayOf(
+//                        "§7-= ${ChatColor.BLUE}KnockIT Stats §7=-",
+//                        "",
+//                        "§7Dein Platz§8: §cLoading...", //TODO: ADD RANG
+//                        "§7Kills§8: $kills",
+//                        "§7Tode§8: $deaths",
+//                        "§7K/D§8: ${(kills.toDouble() / deaths.toDouble()).format(2)}"
+//                ), hologram)
+//                holograms[player.uniqueId]?.show(player)
+//            }
+//        }
+    }
+
+    private fun removeHologram(player: Player) {
+        holograms[player.uniqueId]?.hide(player)
+        holograms.remove(player.uniqueId)
+    }
+
+}
--- Darkness/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/spigot/utils/MapsUtils.kt
+++ Darkness/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/spigot/utils/MapsUtils.kt
@@ -3,11 +3,17 @@
  */
 package net.darkdevelopers.darkbedrock.darkness.spigot.utils
 
+import com.google.gson.JsonArray
+import com.google.gson.JsonObject
+import com.google.gson.JsonPrimitive
 import net.darkdevelopers.darkbedrock.darkness.general.configs.ConfigData
 import net.darkdevelopers.darkbedrock.darkness.general.configs.gson.GsonConfig
+import net.darkdevelopers.darkbedrock.darkness.spigot.configs.gson.BukkitGsonConfig
+import net.darkdevelopers.darkbedrock.darkness.spigot.region.Region
 import org.bukkit.*
 import org.bukkit.entity.EntityType
 import org.bukkit.entity.LivingEntity
+import org.bukkit.entity.Player
 import java.io.File
 import java.util.*
 import kotlin.concurrent.thread
@@ -58,6 +64,61 @@ object MapsUtils {
                 ex.printStackTrace()
             }
             entity.remove()
+        }
+    }
+
+    private fun getMapsAndLoad(config: BukkitGsonConfig, lambda: (Player, MutableMap<UUID, Holograms>) -> Unit): MutableSet<Map> {
+        val maps = mutableSetOf<Map>()
+        val mapsArray = config.getAs<JsonArray>("maps") ?: throw NullPointerException("maps can not be null")
+        mapsArray.forEach {
+            it as JsonObject
+            val name = config.getAs<JsonPrimitive>("name", it)?.asString
+                    ?: throw NullPointerException("map name can not be null")
+            val worldName = config.getAs<JsonPrimitive>("world", it)?.asString
+                    ?: throw NullPointerException("map world can not be null")
+            val region = config.getAs<JsonObject>("region", it)
+                    ?: throw NullPointerException("region jsonObject can not be null")
+            val worldBorder = config.getAs<JsonObject>("worldBoarder", it)
+                    ?: throw NullPointerException("worldBoarder jsonObject can not be null")
+            val world = MapsUtils.loadMap(worldName)
+            setWorldBoarder(worldBorder, world, config)
+            val spawn = config.getLocation("spawn", it, world)
+            val hologram = config.getLocationWithOutYawAndPitch("hologram", it, world)
+            val pos1 = config.getLocationWithOutYawAndPitch("pos1", region, world)
+            val pos2 = config.getLocationWithOutYawAndPitch("pos2", region, world)
+            maps.add(Map(name, spawn, hologram, Region(pos1, pos2), lambda))
+        }
+        return maps
+    }
+
+    private fun setWorldBoarder(jsonObject: JsonObject, world: World, config: BukkitGsonConfig) {
+        val center = config.getAs<JsonObject>("center", jsonObject)
+                ?: throw NullPointerException("center jsonObject can not be null")
+        val damage = config.getAs<JsonObject>("damage", jsonObject)
+                ?: throw NullPointerException("damage jsonObject can not be null")
+        val warning = config.getAs<JsonObject>("warning", jsonObject)
+                ?: throw NullPointerException("warning jsonObject can not be null")
+        val size = config.getAs<JsonPrimitive>("size", jsonObject)?.asDouble
+                ?: throw NullPointerException("worldBorder size can not be null")
+        val x = config.getAs<JsonPrimitive>("x", center)?.asDouble
+                ?: throw NullPointerException("center x can not be null")
+        val z = config.getAs<JsonPrimitive>("z", center)?.asDouble
+                ?: throw NullPointerException("center z can not be null")
+        val buffer = config.getAs<JsonPrimitive>("buffer", damage)?.asDouble
+                ?: throw NullPointerException("damage buffer can not be null")
+        val amount = config.getAs<JsonPrimitive>("amount", damage)?.asDouble
+                ?: throw NullPointerException("damage amount can not be null")
+        val time = config.getAs<JsonPrimitive>("time", warning)?.asInt
+                ?: throw NullPointerException("warning time can not be null")
+        val distance = config.getAs<JsonPrimitive>("distance", warning)?.asInt
+                ?: throw NullPointerException("warning distance can not be null")
+        world.apply {
+            worldBorder.size = size
+            worldBorder.setCenter(x, z)
+            worldBorder.damageBuffer = buffer
+            worldBorder.damageAmount = amount
+            worldBorder.warningDistance = distance
+            worldBorder.warningTime = time
         }
     }
 }
