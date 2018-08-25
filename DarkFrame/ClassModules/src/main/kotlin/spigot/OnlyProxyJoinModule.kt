f07067447d57ea469f9648cd61001a2576b1587c
LartyHD <LartyHD@minewar.de>
2018-08-25 02:14:02 +0200

    Fix OnlyProxyJoin

--- DarkFrame/ClassModules/src/main/kotlin/spigot/OnlyProxyJoinModule.kt
+++ DarkFrame/ClassModules/src/main/kotlin/spigot/OnlyProxyJoinModule.kt
@@ -1,3 +1,4 @@
+
 import com.google.gson.JsonArray
 import com.google.gson.JsonPrimitive
 import net.darkdevelopers.darkbedrock.darkframe.spigot.DarkFrame
@@ -10,20 +11,30 @@ import net.darkdevelopers.darkbedrock.darkness.spigot.listener.Listener
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.player.PlayerLoginEvent
-import java.io.File
 
 /**
  * @author Lars Artmann | LartyHD
  * Created by Lars Artmann | LartyHD on 07.07.2018 19:24.
- * Last edit 07.07.2018
+ * Last edit 25.08.2018
  */
 class OnlyProxyJoinModule : Module, Listener(DarkFrame.instance) {
+
     override val description: ModuleDescription = ModuleDescription("OnlyProxyJoinModule", "1.0", "Lars Artmann | LartyHD", "This module asks if the host address is valid")
 
-    private val config = GsonConfig(ConfigData("modules${File.separator}${description.name}")).load()
-    private val proxies = config.getAs<JsonArray>("proxies").toNonNull()
-    private val ips = mutableSetOf<String>().apply { proxies.forEach { add(it.asString) } }.toSet()
-    private val kickMessage: String = config.getAs<JsonPrimitive>("KickMessage")?.asString.toNonNull()
+    private lateinit var config: GsonConfig
+    private lateinit var proxies: JsonArray
+    private lateinit var ips: Set<String>
+    private lateinit var kickMessage: String
+
+    override fun load() {
+        config = GsonConfig(ConfigData(description.folder)).load()
+    }
+
+    override fun start() {
+        proxies = config.getAs<JsonArray>("proxies").toNonNull()
+        ips = mutableSetOf<String>().apply { proxies.forEach { add(it.asString) } }.toSet()
+        kickMessage = config.getAs<JsonPrimitive>("KickMessage")?.asString.toNonNull()
+    }
 
     @EventHandler(priority = EventPriority.HIGHEST)
     fun onPlayerLoginEvent(event: PlayerLoginEvent) {
