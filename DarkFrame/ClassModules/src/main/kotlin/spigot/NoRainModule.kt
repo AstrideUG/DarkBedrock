4b90e6c08b073ddb02ea9901dbe257d6c90c0f64
LartyHD <LartyHD@minewar.de>
2018-08-26 14:40:06 +0200

    Update NoRainModule

--- DarkFrame/ClassModules/src/main/kotlin/spigot/NoRainModule.kt
+++ DarkFrame/ClassModules/src/main/kotlin/spigot/NoRainModule.kt
@@ -1,4 +1,8 @@
+import com.google.gson.JsonArray
+import com.google.gson.JsonPrimitive
 import net.darkdevelopers.darkbedrock.darkframe.spigot.DarkFrame
+import net.darkdevelopers.darkbedrock.darkness.general.configs.ConfigData
+import net.darkdevelopers.darkbedrock.darkness.general.configs.gson.GsonConfig
 import net.darkdevelopers.darkbedrock.darkness.general.modules.Module
 import net.darkdevelopers.darkbedrock.darkness.general.modules.ModuleDescription
 import net.darkdevelopers.darkbedrock.darkness.spigot.listener.Listener
@@ -11,12 +15,26 @@ import org.bukkit.event.world.WorldLoadEvent
 /**
  * @author Lars Artmann | LartyHD
  * Created by Lars Artmann | LartyHD on 05.07.2018 11:17.
- * Last edit 05.07.2018
+ * Last edit 26.08.2018
  */
 class NoRainModule : Module, Listener(DarkFrame.instance) {
-    override val description: ModuleDescription = ModuleDescription("NoRainModule", "1.1.1", "Lars Artmann | LartyHD", "This modules block rain")
 
-    override fun init() = Bukkit.getWorlds().forEach(this::clearWeather)
+    override val description: ModuleDescription = ModuleDescription("NoRainModule", "1.2", "Lars Artmann | LartyHD", "This modules block rain")
+
+    private lateinit var config: GsonConfig
+
+    override fun load() {
+        config = GsonConfig(ConfigData(description.folder)).load()
+    }
+
+    override fun start() {
+        val worldsWhitelistActive = config.getAsNotNull<JsonPrimitive>("WorldsWhitelistActive").asBoolean
+        if (worldsWhitelistActive) {
+            val rawWorlds = config.getAsNotNull<JsonArray>("Worlds")
+            val worlds = mutableSetOf<World>().apply { rawWorlds.forEach { Bukkit.getWorld(it.asString) } }.toSet()
+            worlds.forEach(this::clearWeather)
+        } else Bukkit.getWorlds().forEach(this::clearWeather)
+    }
 
     @EventHandler
     fun onWeatherChangeEvent(event: WeatherChangeEvent) = cancel(event, event.toWeatherState())
@@ -28,4 +46,5 @@ class NoRainModule : Module, Listener(DarkFrame.instance) {
         world.setStorm(false)
         world.isThundering = false
     }
+
 }
