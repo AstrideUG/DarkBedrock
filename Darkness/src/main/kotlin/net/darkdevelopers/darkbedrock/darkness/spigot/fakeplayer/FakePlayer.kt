946d76bdb3cfa0b4266dbe693b897b262f158b21
LartyHD <LartyHD@minewar.de>
2018-10-16 09:47:37 +0200

    MiniChanges at FakePlayer

--- Darkness/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/spigot/fakeplayer/FakePlayer.kt
+++ Darkness/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/spigot/fakeplayer/FakePlayer.kt
@@ -16,16 +16,16 @@ import java.util.*
 
 /**
  * Created by LartyHD on 17.01.2018  00:30.
- * Last edit 13.03.2018
+ * Last edit 13.03.2018 (16.10.2018 #MiniChange)
  */
 internal class FakePlayer(name: String, private val location: Location) {
     private val entityID: Int = Math.ceil(Math.random() * 1000).toInt() + 2000
-    private val gameprofile: GameProfile = GameProfile(UUID.randomUUID(), name)
+	private val gameProfile: GameProfile = GameProfile(UUID.randomUUID(), name)
 
     fun spawn(player: Player) {
         val packet = PacketPlayOutNamedEntitySpawn()
         setValue(packet, "a", this.entityID)
-        setValue(packet, "b", this.gameprofile.id)
+		setValue(packet, "b", this.gameProfile.id)
         setValue(packet, "c", MathHelper.floor(this.location.x * 32.0))
         setValue(packet, "d", MathHelper.floor(this.location.y * 32.0))
         setValue(packet, "e", MathHelper.floor(this.location.z * 32.0))
@@ -42,7 +42,7 @@ internal class FakePlayer(name: String, private val location: Location) {
     fun spawnAndAddToTabList(player: Player) {
         val packet = PacketPlayOutNamedEntitySpawn()
         setValue(packet, "a", this.entityID)
-        setValue(packet, "b", this.gameprofile.id)
+		setValue(packet, "b", this.gameProfile.id)
         setValue(packet, "c", MathHelper.floor(this.location.x * 32.0))
         setValue(packet, "d", MathHelper.floor(this.location.y * 32.0))
         setValue(packet, "e", MathHelper.floor(this.location.z * 32.0))
@@ -70,9 +70,8 @@ internal class FakePlayer(name: String, private val location: Location) {
 
     private fun addToTabList(player: Player) {
         val packet = PacketPlayOutPlayerInfo()
-        val data = packet.PlayerInfoData(this.gameprofile, 1, EnumGamemode.NOT_SET, CraftChatMessage.fromString(this.gameprofile.name)[0])
-        val players = getValueAs<MutableList<PacketPlayOutPlayerInfo.PlayerInfoData>>(packet, "b") ?: return
-
+		val data = packet.PlayerInfoData(this.gameProfile, 1, EnumGamemode.NOT_SET, CraftChatMessage.fromString(this.gameProfile.name)[0])
+		val players = getValueAs<MutableList<PacketPlayOutPlayerInfo.PlayerInfoData>>(packet, "b") ?: return
         players.add(data)
         setValue(packet, "a", PacketPlayOutPlayerInfo.EnumPlayerInfoAction.ADD_PLAYER)
         setValue(packet, "b", players)
@@ -81,9 +80,8 @@ internal class FakePlayer(name: String, private val location: Location) {
 
     private fun removeFromTabList(player: Player) {
         val packet = PacketPlayOutPlayerInfo()
-        val data = packet.PlayerInfoData(this.gameprofile, 1, EnumGamemode.NOT_SET, CraftChatMessage.fromString(this.gameprofile.name)[0])
-        val players = getValueAs<MutableList<PacketPlayOutPlayerInfo.PlayerInfoData>>(packet, "b") ?: return
-
+		val data = packet.PlayerInfoData(this.gameProfile, 1, EnumGamemode.NOT_SET, CraftChatMessage.fromString(this.gameProfile.name)[0])
+		val players = getValueAs<MutableList<PacketPlayOutPlayerInfo.PlayerInfoData>>(packet, "b") ?: return
         players.add(data)
         setValue(packet, "a", PacketPlayOutPlayerInfo.EnumPlayerInfoAction.REMOVE_PLAYER)
         setValue(packet, "b", players)
