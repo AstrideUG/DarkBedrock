68ca2231045a094298a42ab2a762aab921e39dfe
LartyHD <LartyHD@minewar.de>
2019-02-10 16:31:46 +0100

    Update Modules (Commands) to the new Darkness-Spigot

---
+++ Commands/spigot/src/main/kotlin/HealModule.kt
@@ -0,0 +1,19 @@
+import net.darkdevelopers.darkbedrock.darkframe.spigot.api.DarkFrameSimplePlayerCommandModule
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+
+/**
+ * @author Lars Artmann | LartyHD
+ * Created by Lars Artmann | LartyHD on 22.12.2018 04:46.
+ * Current Version: 1.0 (22.12.2018 - 22.12.2018)
+ */
+class HealModule : DarkFrameSimplePlayerCommandModule("Heal") {
+
+    override fun execute(sender: CommandSender, target: Player) {
+        target.foodLevel = 20
+        target.saturation = 20F
+        target.health = 20.0
+        target.activePotionEffects.forEach { target.removePotionEffect(it.type) }
+    }
+
+}
---
+++ Commands/spigot/src/main/kotlin/InvSeeModule.kt
@@ -0,0 +1,15 @@
+import net.darkdevelopers.darkbedrock.darkframe.spigot.api.DarkFrameSimplePlayerCommandModule
+import net.darkdevelopers.darkbedrock.darkness.spigot.utils.isPlayer
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+
+/**
+ * @author Lars Artmann | LartyHD
+ * Created by Lars Artmann | LartyHD on 22.12.2018 05:31.
+ * Current Version: 1.0 (22.12.2018 - 22.12.2018)
+ */
+class InvSeeModule : DarkFrameSimplePlayerCommandModule("InvSee") {
+
+    override fun execute(sender: CommandSender, target: Player) = sender.isPlayer { it.openInventory(target.inventory) }
+
+}
