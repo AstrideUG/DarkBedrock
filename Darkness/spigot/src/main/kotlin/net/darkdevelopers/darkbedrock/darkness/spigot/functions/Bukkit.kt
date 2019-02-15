a61d87ace4cafecaaeeb1b469062fe66f14873af
LartyHD <LartyHD@minewar.de>
2019-02-15 04:09:10 +0100

    Added fun Cancellable.cancel()

--- Darkness/spigot/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/spigot/functions/Bukkit.kt
+++ Darkness/spigot/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/spigot/functions/Bukkit.kt
@@ -1,11 +1,13 @@
 package net.darkdevelopers.darkbedrock.darkness.spigot.functions
 
 import org.bukkit.Bukkit
+import org.bukkit.GameMode
+import org.bukkit.event.Cancellable
 
 /**
  * @author Lars Artmann | LartyHD
  * Created by Lars Artmann | LartyHD on 22.12.2018 13:59.
- * Current Version: 1.0 (22.12.2018 - 22.12.2018)
+ * Current Version: 1.0 (22.12.2018 - 15.02.2019)
  */
 
 /**
@@ -26,9 +28,30 @@ fun Iterable<String>.toWorlds() = this.mapNotNull {
  * Created by Lars Artmann | LartyHD on 22.12.2018 14:47.
  * Current Version: 1.0 (22.12.2018 - 22.12.2018)
  */
-fun String.toGameMode() = try {
+fun String.toGameMode(): GameMode? = try {
     val id = this.toIntOrNull()
+    @Suppress("DEPRECATION")
     if (id != null) org.bukkit.GameMode.getByValue(id) else org.bukkit.GameMode.valueOf(this.toUpperCase())
 } catch (ex: Exception) {
     null
 }
+
+
+/**
+ * @author Lars Artmann | LartyHD
+ * Created by Lars Artmann | LartyHD on 15.02.2019 04:06.
+ * Current Version: 1.0 (15.02.2019 - 15.02.2019)
+ */
+fun Cancellable.cancel(): Unit = cancel(true)
+
+
+/**
+ * @author Lars Artmann | LartyHD
+ * Created by Lars Artmann | LartyHD on 15.02.2019 04:07.
+ * Current Version: 1.0 (15.02.2019 - 15.02.2019)
+ */
+fun Cancellable.cancel(value: Boolean) {
+    isCancelled = value
+}
+
+
