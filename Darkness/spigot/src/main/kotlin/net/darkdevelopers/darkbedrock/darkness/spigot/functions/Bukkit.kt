115cc9c138b8e31cbfbe80a90186e31c1d45273b
LartyHD <LartyHD@minewar.de>
2019-02-15 05:13:57 +0100

    Added - String.toPlayerUUID() - UUID.toPlayer() - UUID.toOfflinePlayer() to BukkitKT

--- Darkness/spigot/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/spigot/functions/Bukkit.kt
+++ Darkness/spigot/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/spigot/functions/Bukkit.kt
@@ -1,8 +1,12 @@
 package net.darkdevelopers.darkbedrock.darkness.spigot.functions
 
+import net.darkdevelopers.darkbedrock.darkness.general.minecraft.fetcher.Fetcher
 import org.bukkit.Bukkit
 import org.bukkit.GameMode
+import org.bukkit.OfflinePlayer
+import org.bukkit.entity.Player
 import org.bukkit.event.Cancellable
+import java.util.*
 
 /**
  * @author Lars Artmann | LartyHD
@@ -15,7 +19,7 @@ import org.bukkit.event.Cancellable
  * Created by Lars Artmann | LartyHD on 22.12.2018 13:59.
  * Current Version: 1.0 (22.12.2018 - 22.12.2018)
  */
-fun Iterable<String>.toWorlds() = this.mapNotNull {
+fun Iterable<String>.toWorlds() = mapNotNull {
     try {
         Bukkit.getWorld(it)
     } catch (ex: Exception) {
@@ -29,13 +33,37 @@ fun Iterable<String>.toWorlds() = this.mapNotNull {
  * Current Version: 1.0 (22.12.2018 - 22.12.2018)
  */
 fun String.toGameMode(): GameMode? = try {
-    val id = this.toIntOrNull()
+    val id = toIntOrNull()
     @Suppress("DEPRECATION")
-    if (id != null) org.bukkit.GameMode.getByValue(id) else org.bukkit.GameMode.valueOf(this.toUpperCase())
+    if (id != null) org.bukkit.GameMode.getByValue(id) else org.bukkit.GameMode.valueOf(toUpperCase())
 } catch (ex: Exception) {
     null
 }
 
+/**
+ * @author Lars Artmann | LartyHD
+ * Created by Lars Artmann | LartyHD on 15.02.2019 05:08.
+ * Current Version: 1.0 (15.02.2019 - 15.02.2019)
+ */
+fun String.toPlayerUUID() = try {
+    UUID.fromString(this)
+} catch (ex: Exception) {
+    Bukkit.getPlayer(this)?.uniqueId ?: Fetcher.getUUID(this)
+}
+
+/**
+ * @author Lars Artmann | LartyHD
+ * Created by Lars Artmann | LartyHD on 15.02.2019 05:09.
+ * Current Version: 1.0 (15.02.2019 - 15.02.2019)
+ */
+fun UUID.toPlayer(): Player? = Bukkit.getPlayer(this)
+
+/**
+ * @author Lars Artmann | LartyHD
+ * Created by Lars Artmann | LartyHD on 15.02.2019 05:10.
+ * Current Version: 1.0 (15.02.2019 - 15.02.2019)
+ */
+fun UUID.toOfflinePlayer(): OfflinePlayer? = Bukkit.getOfflinePlayer(this)
 
 /**
  * @author Lars Artmann | LartyHD
