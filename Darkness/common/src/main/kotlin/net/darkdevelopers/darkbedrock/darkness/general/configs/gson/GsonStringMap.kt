8d65b8fbbb4532759066892342c69ed513b6aa68
LartyHD <lartyhd@gmail.com>
2019-04-05 20:35:49 +0200

    Changed GsonStringMap available to Map<String, List<String?>>

--- Darkness/common/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/general/configs/gson/GsonStringMap.kt
+++ Darkness/common/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/general/configs/gson/GsonStringMap.kt
@@ -4,22 +4,23 @@
 
 package net.darkdevelopers.darkbedrock.darkness.general.configs.gson
 
+import com.google.gson.JsonArray
 import com.google.gson.JsonObject
 import net.darkdevelopers.darkbedrock.darkness.general.functions.asString
 
 /**
  * @author Lars Artmann | LartyHD
  * Created by Lars Artmann | LartyHD on 25.08.2018 00:37.
- * Last edit 18.03.2019
+ * Last edit 05.04.2019
  */
 open class GsonStringMap(jsonObject: JsonObject) {
 
     /**
      * @author Lars Artmann | LartyHD
      * Created by Lars Artmann | LartyHD on 18.03.2019 21:40.
-     * Current Version: 1.0 (18.03.2019 - 18.03.2019)
+     * Current Version: 1.0 (18.03.2019 - 05.04.2019)
      */
-    open val available: Map<String, String?> = jsonObject.toStringMap()
+    open val available: Map<String, List<String?>> = jsonObject.toStringMap()
 
 //    /**
 //     * @author Lars Artmann | LartyHD
@@ -35,11 +36,14 @@ open class GsonStringMap(jsonObject: JsonObject) {
 /**
  * @author Lars Artmann | LartyHD
  * Created by Lars Artmann | LartyHD on 18.03.2019 22:43.
- * Current Version: 1.0 (18.03.2019 - 18.03.2019)
+ * Current Version: 1.0 (18.03.2019 - 05.04.2019)
  */
-private fun JsonObject.toStringMap(): Map<String, String?> = entrySet().mapNotNull { (key, element) ->
+private fun JsonObject.toStringMap(): Map<String, List<String?>> = entrySet().mapNotNull { (key, element) ->
     try {
-        key to element.asString()
+        val single = element.asString()
+        val jsonArray = element as? JsonArray
+
+        key to if (single == null) jsonArray?.map { it.asString() } ?: listOf(null) else listOf(single)
     } catch (e: Exception) {
         null
     }
