572311003a02e8eed33a15b6583d3e6bd930abb5
LartyHD <lartyhd@gmail.com>
2019-04-05 20:40:08 +0200

    Added Array<String?> & Collection<String?> send(IfNotNul/To)

--- Darkness/spigot/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/spigot/functions/CommandSender.kt
+++ Darkness/spigot/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/spigot/functions/CommandSender.kt
@@ -7,6 +7,12 @@ package net.darkdevelopers.darkbedrock.darkness.spigot.functions
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 
+/*
+ * @author Lars Artmann | LartyHD
+ * Created by Lars Artmann | LartyHD on 31.08.2018 19:15.
+ * Last edit 05.04.2019
+ */
+
 /**
  * @author Lars Artmann | LartyHD
  * Created by Lars Artmann | LartyHD on 31.08.2018 19:15.
@@ -21,16 +27,47 @@ fun CommandSender.sendConfigurableMessage(name: String): Unit = sendMessage(mess
 /**
  * @author Lars Artmann | LartyHD
  * Created by Lars Artmann | LartyHD on 21.03.2019 02:39.
- * Current Version: 1.0 (21.03.2019 - 21.03.2019)
+ * Current Version: 1.0 (21.03.2019 - 05.04.2019)
  */
-fun String?.sendIfNotNull(sender: CommandSender) = this?.sendTo(sender)
+fun String?.sendIfNotNull(sender: CommandSender) {
+    this?.sendTo(sender)
+}
+
+/**
+ * @author Lars Artmann | LartyHD
+ * Created by Lars Artmann | LartyHD on 05.04.2019 20:32.
+ * Current Version: 1.0 (05.04.2019 - 05.04.2019)
+ */
+fun Array<String?>.sendIfNotNull(sender: CommandSender): Unit = mapNotNull { it }.sendTo(sender)
+
+/**
+ * @author Lars Artmann | LartyHD
+ * Created by Lars Artmann | LartyHD on 05.04.2019 20:25.
+ * Current Version: 1.0 (05.04.2019 - 05.04.2019)
+ */
+fun Collection<String?>.sendIfNotNull(sender: CommandSender): Unit = mapNotNull { it }.sendTo(sender)
 
 /**
  * @author Lars Artmann | LartyHD
  * Created by Lars Artmann | LartyHD on 21.03.2019 02:39.
  * Current Version: 1.0 (21.03.2019 - 21.03.2019)
  */
-fun String.sendTo(sender: CommandSender) = sender.sendMessage(this)
+fun String.sendTo(sender: CommandSender): Unit = sender.sendMessage(this)
+
+/**
+ * @author Lars Artmann | LartyHD
+ * Created by Lars Artmann | LartyHD on 05.04.2019 20:29.
+ * Current Version: 1.0 (05.04.2019 - 05.04.2019)
+ */
+fun Array<String?>.sendTo(sender: CommandSender): Unit = sender.sendMessage(this)
+
+/**
+ * @author Lars Artmann | LartyHD
+ * Created by Lars Artmann | LartyHD on 05.04.2019 20:31.
+ * Current Version: 1.0 (05.04.2019 - 05.04.2019)
+ */
+fun Collection<String?>.sendTo(sender: CommandSender): Unit = sender.sendMessage(toTypedArray())
+
 
 /**
  * @author Lars Artmann | LartyHD
@@ -42,10 +79,10 @@ var isPlayerFailedMessage: String = "The command is only for players"
 /**
  * @author Lars Artmann | LartyHD
  * Created by Lars Artmann | LartyHD on 21.03.2019 02:35.
- * Current Version: 1.0 (21.03.2019 - 21.03.2019)
+ * Current Version: 1.0 (21.03.2019 - 05.04.2019)
  */
-inline fun CommandSender.isPlayer(lambda: (Player) -> Unit): Unit =
-    isPlayer(lambda) { sendMessage(isPlayerFailedMessage) }
+inline fun CommandSender.isPlayer(lambda: (Player) -> Unit, isNoPlayerMessage: String = isPlayerFailedMessage): Unit =
+    isPlayer(lambda) { sendMessage(isNoPlayerMessage) }
 
 /**
  * @author Lars Artmann | LartyHD
