9a6dba3dff9b68a51321f072928a4df952a50c4a
LartyHD <lartyhd@gmail.com>
2019-04-06 08:04:23 +0200

    Added map parameter to utils.Map

--- Darkness/spigot/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/spigot/utils/Map.kt
+++ Darkness/spigot/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/spigot/utils/Map.kt
@@ -13,34 +13,21 @@ import java.util.*
 /**
  * @author Lars Artmann | LartyHD
  * Created by Lars Artmann | LartyHD on 12.08.2018 20:49.
- * Last edit 15.08.2018
+ * Last edit 06.04.2019
  */
 data class Map(
     val name: String,
     val spawn: Location,
     val hologram: Location,
     val region: Region,
-    val sendHolograms: (Player, MutableMap<UUID, Holograms>) -> Unit
+    val sendHolograms: (Player, MutableMap<UUID, Holograms>, map: Map) -> Unit
 ) {
 
     private val holograms = mutableMapOf<UUID, Holograms>()
 
     fun sendHologram(player: Player) {
         removeHologram(player)
-        sendHolograms(player, holograms)
-//        KnockIT.instance.stats.get(player.uniqueId, "kills") { kills ->
-//            KnockIT.instance.stats.get(player.uniqueId, "deaths") { deaths ->
-//                holograms[player.uniqueId] = Holograms(arrayOf(
-//                        "§7-= ${ChatColor.BLUE}KnockIT Stats §7=-",
-//                        "",
-//                        "§7Dein Platz§8: §cLoading...", //TODO: ADD RANG
-//                        "§7Kills§8: $kills",
-//                        "§7Tode§8: $deaths",
-//                        "§7K/D§8: ${(kills.toDouble() / deaths.toDouble()).format(2)}"
-//                ), hologram)
-//                holograms[player.uniqueId]?.show(player)
-//            }
-//        }
+        sendHolograms(player, holograms, this)
     }
 
     private fun removeHologram(player: Player) {
