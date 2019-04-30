670cbf39e654b72d8293e52ad6a40497f4e29db4
LartyHD <lartyhd@gmail.com>
2019-04-30 02:56:17 +0200

    Add count((T) -> Int)

---
+++ Darkness/common/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/general/functions/Collections.kt
@@ -0,0 +1,20 @@
+/*
+ * © Copyright - Lars Artmann aka. LartyHD 2019.
+ */
+
+package net.darkdevelopers.darkbedrock.darkness.general.functions
+
+//TODO: Add it to Kotlin
+
+/**
+ * @author Lars Artmann | LartyHD
+ * Created by Lars Artmann | LartyHD on 30.04.2019 02:27.
+ * Current Version: 1.0 (30.04.2019 - 30.04.2019)
+ */
+@JvmName("countWith")
+inline fun <T> Iterable<T>.count(predicate: (T) -> Int): Int {
+    if (this is Collection && isEmpty()) return 0
+    var count = 0
+    for (element in this) count += predicate(element)
+    return count
+}
