483b0e5030138f943edee871902656e27a7409ed
LartyHD <lartyhd@gmail.com>
2019-05-06 20:44:40 +0200

    Improved ReflectUtils

--- Darkness/common/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/general/utils/ReflectUtils.kt
+++ Darkness/common/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/general/utils/ReflectUtils.kt
@@ -7,21 +7,21 @@ import java.lang.reflect.Field
 import java.lang.reflect.Modifier
 
 object ReflectUtils {
-    fun setValue(any: Any, name: String, value: Any) = try {
+    fun setValue(any: Any, name: String, value: Any): Unit? = try {
         getField(any.javaClass, name)?.set(any, value)
     } catch (ex: Exception) {
         ex.printStackTrace()
     }
 
     @Deprecated("Use getValue(Class<*>, Any, String)", ReplaceWith("getValue(any.javaClass, any, name)"))
-    fun getValue(any: Any, name: String) = try {
+    fun getValue(any: Any, name: String): Any? = try {
         getField(any.javaClass, name)?.get(any)
     } catch (ex: Exception) {
         ex.printStackTrace()
         null
     }
 
-    fun getValue(clazz: Class<*>, any: Any, name: String) = try {
+    fun getValue(clazz: Class<*>, any: Any, name: String): Any? = try {
         getField(clazz, name)?.get(any)
     } catch (ex: Exception) {
         ex.printStackTrace()
@@ -29,11 +29,11 @@ object ReflectUtils {
     }
 
     @Suppress("UNCHECKED_CAST")
-    fun <O : Any?> getValueAs(any: Any, name: String) = getValue(any.javaClass, any, name) as? O
+    fun <O : Any?> getValueAs(any: Any, name: String): O? = getValue(any.javaClass, any, name) as? O
 
     @Suppress("MemberVisibilityCanBePrivate")
     fun getField(clazz: Class<*>, name: String): Field? = try {
-        val field = getAccessibleField(clazz, name)
+        val field = clazz.getAccessibleField(name)
         if (Modifier.isFinal(field.modifiers))
             getField(Field::class.java, "modifiers")?.set(field, field.modifiers and Modifier.FINAL.inv())
         field
@@ -42,17 +42,13 @@ object ReflectUtils {
         null
     }
 
-    private fun getAccessibleField(clazz: Class<*>, name: String): Field {
-        val field = clazz.getDeclaredField(name)
-        field.isAccessible = true
-        return field
-    }
 }
 
-fun Any.setValue(name: String, value: Any) = printException { javaClass.getModifiableField(name)?.set(this, value) }
+fun Any.setValue(name: String, value: Any): Unit =
+    printException { javaClass.getModifiableField(name)?.set(this, value) }
 
 @Suppress("UNCHECKED_CAST")
-fun <O : Any?> Any.getValueAs(name: String) = this.getValue(name) as? O
+fun <O : Any?> Any.getValueAs(name: String): O? = this.getValue(name) as? O
 
 fun Any.getValue(name: String): Any? {
     printException { return javaClass.getModifiableField(name)?.get(this) }
@@ -70,7 +66,7 @@ fun <T : Any> Class<T>.getModifiableField(name: String): Field? {
 }
 
 private fun <T : Any> Class<T>.getAccessibleField(name: String): Field =
-    getDeclaredField(name).apply { isAccessible = true }
+    getField(name).apply { isAccessible = true }
 
 private inline fun printException(lambda: () -> Unit) = try {
     lambda()
