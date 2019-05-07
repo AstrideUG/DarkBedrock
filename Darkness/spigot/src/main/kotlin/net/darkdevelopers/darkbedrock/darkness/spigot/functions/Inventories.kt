665ba9deec34421eccd990b497774cecf4dcc81b
LartyHD <lartyhd@gmail.com>
2019-05-07 17:45:44 +0200

    Added Iterable<Inventory>.listen*

--- Darkness/spigot/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/spigot/functions/Inventories.kt
+++ Darkness/spigot/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/spigot/functions/Inventories.kt
@@ -21,6 +21,104 @@ import org.bukkit.plugin.Plugin
  * Current Version: 1.0 (07.05.2019 - 07.05.2019)
  */
 
+
+inline fun Iterable<Inventory>.listenTop(
+    plugin: Plugin,
+    crossinline acceptWhoClicked: (HumanEntity) -> Boolean = { true },
+    crossinline acceptCurrentItem: (ItemStack?) -> Boolean = { it != null },
+    crossinline acceptSlot: (Int) -> Boolean = { true },
+    crossinline acceptSlotType: (InventoryType.SlotType) -> Boolean = { true },
+    crossinline acceptClick: (ClickType) -> Boolean = { true },
+    crossinline acceptAction: (InventoryAction) -> Boolean = { true },
+    crossinline acceptCursor: (ItemStack?) -> Boolean = { true },
+    crossinline acceptHotBarButton: (Int) -> Boolean = { true },
+    crossinline acceptIsLeftClick: (Boolean) -> Boolean = { true },
+    crossinline acceptIsRightClick: (Boolean) -> Boolean = { true },
+    crossinline acceptIsShiftClick: (Boolean) -> Boolean = { true },
+    crossinline block: (InventoryClickEvent) -> Unit
+): Listener = listenInventories(
+    plugin,
+    acceptWhoClicked,
+    acceptCurrentItem,
+    acceptSlot,
+    acceptSlotType,
+    acceptClick,
+    acceptAction,
+    acceptCursor,
+    acceptHotBarButton,
+    acceptIsLeftClick,
+    acceptIsRightClick,
+    acceptIsShiftClick
+) { event ->
+    if (event.clickedInventory !in this) return@listenInventories
+    if (event.whoClicked.openInventory.topInventory != event.clickedInventory) return@listenInventories
+    block(event)
+}
+
+inline fun Iterable<Inventory>.listenBottom(
+    plugin: Plugin,
+    crossinline acceptWhoClicked: (HumanEntity) -> Boolean = { true },
+    crossinline acceptCurrentItem: (ItemStack?) -> Boolean = { it != null },
+    crossinline acceptSlot: (Int) -> Boolean = { true },
+    crossinline acceptSlotType: (InventoryType.SlotType) -> Boolean = { true },
+    crossinline acceptClick: (ClickType) -> Boolean = { true },
+    crossinline acceptAction: (InventoryAction) -> Boolean = { true },
+    crossinline acceptCursor: (ItemStack?) -> Boolean = { true },
+    crossinline acceptHotBarButton: (Int) -> Boolean = { true },
+    crossinline acceptIsLeftClick: (Boolean) -> Boolean = { true },
+    crossinline acceptIsRightClick: (Boolean) -> Boolean = { true },
+    crossinline acceptIsShiftClick: (Boolean) -> Boolean = { true },
+    crossinline block: (InventoryClickEvent) -> Unit
+): Listener = listenInventories(
+    plugin,
+    acceptWhoClicked,
+    acceptCurrentItem,
+    acceptSlot,
+    acceptSlotType,
+    acceptClick,
+    acceptAction,
+    acceptCursor,
+    acceptHotBarButton,
+    acceptIsLeftClick,
+    acceptIsRightClick,
+    acceptIsShiftClick
+) { event ->
+    if (event.clickedInventory !in this) return@listenInventories
+    if (event.whoClicked.openInventory.bottomInventory != event.clickedInventory) return@listenInventories
+    block(event)
+}
+
+inline fun Iterable<Inventory>.listenClicked(
+    plugin: Plugin,
+    crossinline acceptWhoClicked: (HumanEntity) -> Boolean = { true },
+    crossinline acceptCurrentItem: (ItemStack?) -> Boolean = { it != null },
+    crossinline acceptSlot: (Int) -> Boolean = { true },
+    crossinline acceptSlotType: (InventoryType.SlotType) -> Boolean = { true },
+    crossinline acceptClick: (ClickType) -> Boolean = { true },
+    crossinline acceptAction: (InventoryAction) -> Boolean = { true },
+    crossinline acceptCursor: (ItemStack?) -> Boolean = { true },
+    crossinline acceptHotBarButton: (Int) -> Boolean = { true },
+    crossinline acceptIsLeftClick: (Boolean) -> Boolean = { true },
+    crossinline acceptIsRightClick: (Boolean) -> Boolean = { true },
+    crossinline acceptIsShiftClick: (Boolean) -> Boolean = { true },
+    crossinline block: (InventoryClickEvent) -> Unit
+): Listener = listenInventories(
+    plugin,
+    acceptWhoClicked,
+    acceptCurrentItem,
+    acceptSlot,
+    acceptSlotType,
+    acceptClick,
+    acceptAction,
+    acceptCursor,
+    acceptHotBarButton,
+    acceptIsLeftClick,
+    acceptIsRightClick,
+    acceptIsShiftClick
+) { event ->
+    if (event.clickedInventory in this) block(event)
+}
+
 inline fun Inventory.listenTop(
     plugin: Plugin,
     crossinline acceptWhoClicked: (HumanEntity) -> Boolean = { true },
@@ -49,7 +147,9 @@ inline fun Inventory.listenTop(
     acceptIsRightClick,
     acceptIsShiftClick
 ) { event ->
-    if (event.whoClicked.openInventory.topInventory == this) block(event)
+    if (event.clickedInventory != this) return@listenInventories
+    if (event.whoClicked.openInventory.topInventory != event.clickedInventory) return@listenInventories
+    block(event)
 }
 
 inline fun Inventory.listenBottom(
@@ -80,7 +180,9 @@ inline fun Inventory.listenBottom(
     acceptIsRightClick,
     acceptIsShiftClick
 ) { event ->
-    if (event.whoClicked.openInventory.bottomInventory == this) block(event)
+    if (event.clickedInventory != this) return@listenInventories
+    if (event.whoClicked.openInventory.bottomInventory != event.clickedInventory) return@listenInventories
+    block(event)
 }
 
 inline fun Inventory.listenClicked(
