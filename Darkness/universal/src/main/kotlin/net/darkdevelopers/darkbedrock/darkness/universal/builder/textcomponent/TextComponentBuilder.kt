51c887b1c394059576c7c1878c8d686e95af4bf1
LartyHD <lartyhd@gmail.com>
2019-05-07 22:42:05 +0200

    Improved TextComponentBuilder code

--- Darkness/universal/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/universal/builder/textcomponent/TextComponentBuilder.kt
+++ Darkness/universal/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/universal/builder/textcomponent/TextComponentBuilder.kt
@@ -12,31 +12,21 @@ import net.md_5.bungee.api.chat.TextComponent
 /**
  * @author Lars Artmann | LartyHD
  * Created by Lars Artmann | LartyHD on 28.08.2018 23:40.
- * Last edit 28.08.2018
+ * Last edit 07.05.2019
  */
 private class TextComponentBuilder(private val textComponent: TextComponent) : ITextComponentBuilder {
 
-	override fun setText(text: String): ITextComponentBuilder {
-		textComponent.text = text
-		return this
-	}
+    override fun setText(text: String): ITextComponentBuilder = apply { textComponent.text = text }
 
-	override fun setColor(color: ChatColor): ITextComponentBuilder {
-		textComponent.color = color
-		return this
-	}
+    override fun setColor(color: ChatColor): ITextComponentBuilder = apply { textComponent.color = color }
 
-	override fun setClickEvent(clickEvent: ClickEvent): ITextComponentBuilder {
-		textComponent.clickEvent = clickEvent
-		return this
-	}
+    override fun setClickEvent(clickEvent: ClickEvent): ITextComponentBuilder =
+        apply { textComponent.clickEvent = clickEvent }
 
-	override fun setHoverEvent(hoverEvent: HoverEvent): ITextComponentBuilder {
-		textComponent.hoverEvent = hoverEvent
-		return this
-	}
+    override fun setHoverEvent(hoverEvent: HoverEvent): ITextComponentBuilder =
+        apply { textComponent.hoverEvent = hoverEvent }
 
-	override fun build() = textComponent
+    override fun build() = textComponent
 }
 
 fun TextComponent.builder(): ITextComponentBuilder = TextComponentBuilder(this)
