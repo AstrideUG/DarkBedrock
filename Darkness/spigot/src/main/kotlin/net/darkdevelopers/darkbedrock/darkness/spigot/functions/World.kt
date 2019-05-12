4cc9937276b5e0d48407735627af3b1373850103
LartyHD <lartyhd@gmail.com>
2019-05-12 22:31:14 +0200

    Added String.toBukkitWorld()

--- Darkness/spigot/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/spigot/functions/World.kt
+++ Darkness/spigot/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/spigot/functions/World.kt
@@ -1,6 +1,7 @@
 package net.darkdevelopers.darkbedrock.darkness.spigot.functions
 
 import net.darkdevelopers.darkbedrock.darkness.spigot.location.vector.Vector2D
+import org.bukkit.Bukkit
 import org.bukkit.Difficulty
 import org.bukkit.World
 
@@ -66,3 +67,9 @@ fun World.setWorldBorder(
     worldBorder.warningTime = time
 }
 
+/**
+ * @author Lars Artmann | LartyHD
+ * Created by Lars Artmann | LartyHD on 12.05.2019 22:29.
+ * Current Version: 1.0 (12.05.2019 - 12.05.2019)
+ */
+fun String.toBukkitWorld(): World? = Bukkit.getWorld(this)
