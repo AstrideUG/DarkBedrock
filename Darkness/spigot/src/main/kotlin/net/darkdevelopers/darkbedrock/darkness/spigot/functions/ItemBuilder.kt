205986e2dd98c2d82148f8da0e59efb6e405e643
LartyHD <lartyhd@gmail.com>
2019-05-12 19:29:31 +0200

    Added ItemBuilder.glow() #28

---
+++ Darkness/spigot/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/spigot/functions/ItemBuilder.kt
@@ -0,0 +1,18 @@
+package net.darkdevelopers.darkbedrock.darkness.spigot.functions
+
+import net.darkdevelopers.darkbedrock.darkness.spigot.builder.item.interfaces.IItemBuilder
+import org.bukkit.enchantments.Enchantment
+import org.bukkit.inventory.ItemFlag
+
+/*
+ * @author Lars Artmann | LartyHD
+ * Created by Lars Artmann | LartyHD on 12.05.2019 19:28.
+ * Current Version: 1.0 (12.05.2019 - 12.05.2019)
+ */
+
+/**
+ * @author Lars Artmann | LartyHD
+ * Created by Lars Artmann | LartyHD on 12.05.2019 19:28.
+ * Current Version: 1.0 (12.05.2019 - 12.05.2019)
+ */
+fun IItemBuilder.glow(): IItemBuilder = addEnchant(Enchantment.LUCK, 10).addItemFlags(ItemFlag.HIDE_ENCHANTS)
