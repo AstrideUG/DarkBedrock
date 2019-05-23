53e6d5d829c55d74153bf019bbfe3ff7e68500ea
LartyHD <lartyhd@gmail.com>
2019-05-24 00:00:44 +0200

    Added (Mapper-) & CheckByCollection

---
+++ Darkness/common/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/general/delegate/CheckByCollection.kt
@@ -0,0 +1,22 @@
+/*
+ * © Copyright - Lars Artmann aka. LartyHD 2019.
+ */
+
+package net.darkdevelopers.darkbedrock.darkness.general.delegate
+
+import kotlin.reflect.KProperty
+
+/**
+ * @author Lars Artmann | LartyHD
+ * Created by Lars Artmann | LartyHD on 23.05.2019 23:46.
+ * Last edit 23.05.2019
+ */
+@Suppress("unused")
+class CheckByCollection<T : Any?>(private val collection: MutableCollection<T>) {
+
+    operator fun getValue(thisRef: T, property: KProperty<*>): Boolean = thisRef in collection
+
+    operator fun setValue(thisRef: T, property: KProperty<*>, value: Boolean): Unit =
+        if (value) collection += thisRef else collection -= thisRef
+
+}
---
+++ Darkness/common/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/general/delegate/MapperCheckByCollection.kt
@@ -0,0 +1,27 @@
+/*
+ * © Copyright - Lars Artmann aka. LartyHD 2019.
+ */
+
+package net.darkdevelopers.darkbedrock.darkness.general.delegate
+
+import kotlin.reflect.KProperty
+
+/**
+ * @author Lars Artmann | LartyHD
+ * Created by Lars Artmann | LartyHD on 23.05.2019 23:54.
+ * Current Version: 1.0 (23.05.2019 - 23.05.2019)
+ */
+@Suppress("unused")
+class MapperCheckByCollection<I : Any?, C : Any?>(
+    private val collection: MutableCollection<C>,
+    private val mapped: I.() -> C
+) {
+
+    operator fun getValue(thisRef: I, property: KProperty<*>): Boolean = thisRef.mapped() in collection
+
+    operator fun setValue(thisRef: I, property: KProperty<*>, value: Boolean) {
+        val mapped: C = thisRef.mapped()
+        if (value) collection += mapped else collection -= mapped
+    }
+
+}
