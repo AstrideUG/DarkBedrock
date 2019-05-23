8ad624bdd20771fd5b730bbb2513da9f50213404
LartyHD <lartyhd@gmail.com>
2019-05-23 19:02:25 +0200

    Spited Any?.toJsonElement to Any?.toJsonElement(), Iterable<*>.toJsonArray() and Map<*, Any?>.toJsonObject() #76

--- Darkness/spigot/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/spigot/functions/Json.kt
+++ Darkness/spigot/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/spigot/functions/Json.kt
@@ -76,10 +76,25 @@ fun JsonElement.toObject(): Any? = when (this) {
 
 fun Any?.toJsonElement(serializeNull: Boolean = false): JsonElement? = when (this) {
     null -> if (serializeNull) JsonNull.INSTANCE else null
-    is Iterable<*> -> JsonArray(this.mapNotNull { it?.toJsonElement() })
-    is Map<*, Any?> -> JsonObject(this.mapNotNull { (it.key as? String to it.value.toJsonElement()).toNotNull() }.toMap())
+    is Iterable<*> -> this.toJsonArray()
+    is Map<*, Any?> -> this.toJsonObject()
     else -> toJsonPrimitive()
 }
+
+/**
+ * @author Lars Artmann | LartyHD
+ * Created by Lars Artmann | LartyHD on 23.05.2019 18:58.
+ * Current Version: 1.0 (23.05.2019 - 23.05.2019)
+ */
+fun Iterable<*>.toJsonArray(): JsonArray = JsonArray(this.mapNotNull { it?.toJsonElement() })
+
+/**
+ * @author Lars Artmann | LartyHD
+ * Created by Lars Artmann | LartyHD on 23.05.2019 18:59.
+ * Current Version: 1.0 (23.05.2019 - 23.05.2019)
+ */
+fun Map<*, Any?>.toJsonObject(): JsonObject =
+    JsonObject(this.mapNotNull { (it.key as? String to it.value.toJsonElement()).toNotNull() }.toMap())
 
 /**
  * @author Lars Artmann | LartyHD
