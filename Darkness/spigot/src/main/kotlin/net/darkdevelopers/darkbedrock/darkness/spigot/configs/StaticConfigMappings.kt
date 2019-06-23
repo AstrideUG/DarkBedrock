6f25efdee05f32a5d5ba9d694ad5f33f5c1e4c88
LartyHD <lartyhd@gmail.com>
2019-06-24 00:02:36 +0200

    Add Iterable<KMutableProperty0<*>>.createConfigs(File) #143

--- Darkness/spigot/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/spigot/configs/StaticConfigMappings.kt
+++ Darkness/spigot/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/spigot/configs/StaticConfigMappings.kt
@@ -4,8 +4,14 @@
 
 package net.darkdevelopers.darkbedrock.darkness.spigot.configs
 
+import com.google.gson.JsonObject
 import net.darkdevelopers.darkbedrock.darkness.general.configs.defaultMappings
 import net.darkdevelopers.darkbedrock.darkness.general.configs.mapped
+import net.darkdevelopers.darkbedrock.darkness.general.configs.toConfigMap
+import net.darkdevelopers.darkbedrock.darkness.general.functions.load
+import net.darkdevelopers.darkbedrock.darkness.general.functions.save
+import net.darkdevelopers.darkbedrock.darkness.general.functions.toConfigData
+import net.darkdevelopers.darkbedrock.darkness.general.functions.toMap
 import net.darkdevelopers.darkbedrock.darkness.spigot.functions.toMaterial
 import net.darkdevelopers.darkbedrock.darkness.spigot.location.extensions.BukkitLocation
 import net.darkdevelopers.darkbedrock.darkness.spigot.location.extensions.toBukkitLocation
@@ -13,6 +19,10 @@ import net.darkdevelopers.darkbedrock.darkness.spigot.location.location.inmutabl
 import net.darkdevelopers.darkbedrock.darkness.spigot.location.location.inmutable.extensions.alliases.DefaultLivingLocation
 import net.darkdevelopers.darkbedrock.darkness.spigot.location.location.inmutable.extensions.serialization.deserialization.toLookableLocation
 import org.bukkit.Material
+import java.io.File
+import kotlin.reflect.KMutableProperty0
+import kotlin.reflect.jvm.jvmErasure
+import kotlin.reflect.typeOf
 
 /*
  * @author Lars Artmann | LartyHD
@@ -31,3 +41,21 @@ fun initSpigotStaticConfigMappings() {
 }
 
 fun resetSpigotStaticConfigMappings(): Unit = spigotDefaultMappings.keys.forEach { defaultMappings -= it }
+
+@ExperimentalStdlibApi
+fun Iterable<KMutableProperty0<*>>.createConfigs(directory: File): Unit = forEach { property ->
+
+    val configData = property.name.toLowerCase().toConfigData(directory)
+    val values = configData.load<JsonObject>().toMap()
+
+    val createType = property.returnType.jvmErasure
+    val constructor = createType.constructors.find {
+        it.parameters.singleOrNull()?.type == typeOf<Map<String, Any?>>()
+    } ?: return@forEach
+
+    val instance = constructor.call(values)
+    property.setter.call(instance)
+
+    configData.save(instance.toConfigMap())
+
+}
