3a5bfa44584df6396174cf77f96411b20447e38d
LartyHD <lartyhd@gmail.com>
2019-06-23 04:05:41 +0200

    Improved MongoData

--- Darkness/common/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/general/databases/mongodb/MongoData.kt
+++ Darkness/common/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/general/databases/mongodb/MongoData.kt
@@ -4,15 +4,18 @@
 
 package net.darkdevelopers.darkbedrock.darkness.general.databases.mongodb
 
+import net.darkdevelopers.darkbedrock.darkness.general.configs.default
+import net.darkdevelopers.darkbedrock.darkness.general.configs.getValue
+
 /**
+ * Created on 05.06.2019 00:14.
  * @author Lars Artmann | LartyHD
- * Created by Lars Artmann | LartyHD on 05.06.2019 00:14.
- * Current Version: 1.0 (05.06.2019 - 05.06.2019)
  */
-data class MongoData(
-    val host: String = "localhost",
-    val port: Int = 27017,
-    val username: String = "",
-    val password: String = "",
-    val simple: Boolean = true
-)
+class MongoData(values: Map<String, Any?>) {
+    val host: String by values.default { "localhost" }
+    val port: Int by values.default { 27017 }
+    val username: String by values.default { "" }
+    val password: String by values.default { "" }
+    val simple: Boolean by values.default { true }
+}
+
