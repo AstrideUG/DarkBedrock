bbb7dd686e60a25b2197a55edee9c7cf2b141723
LartyHD <lartyhd@gmail.com>
2019-06-24 09:40:49 +0200

    Added Locations extension functions (from BedWars)

---
+++ Darkness/spigot/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/spigot/location/location/inmutable/extensions/to/ToBukkitLocation.kt
@@ -0,0 +1,24 @@
+/*
+ * © Copyright by Astride UG (haftungsbeschränkt) 2018 - 2019.
+ */
+
+package net.darkdevelopers.darkbedrock.darkness.spigot.location.location.inmutable.extensions.to
+
+import net.darkdevelopers.darkbedrock.darkness.spigot.functions.toBukkitWorld
+import net.darkdevelopers.darkbedrock.darkness.spigot.location.extensions.BukkitLocation
+import net.darkdevelopers.darkbedrock.darkness.spigot.location.extensions.toLocation
+import net.darkdevelopers.darkbedrock.darkness.spigot.location.location.inmutable.extensions.alliases.DefaultBlockLocation
+import net.darkdevelopers.darkbedrock.darkness.spigot.location.location.inmutable.extensions.alliases.x
+import net.darkdevelopers.darkbedrock.darkness.spigot.location.location.inmutable.extensions.alliases.y
+import net.darkdevelopers.darkbedrock.darkness.spigot.location.location.inmutable.extensions.alliases.z
+
+/*
+ * Created on 24.06.2019 09:38.
+ * @author Lars Artmann | LartyHD
+ */
+
+fun DefaultBlockLocation.toBukkitLocation(): BukkitLocation = BukkitLocation(
+    world.toBukkitWorld(), x.toDouble(), y.toDouble(), z.toDouble()
+)
+
+fun BukkitLocation.toDefaultBlockLocation(): DefaultBlockLocation = toLocation().toLocation3I()
--- Darkness/spigot/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/spigot/location/location/inmutable/extensions/to/ToLocation.kt
+++ Darkness/spigot/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/spigot/location/location/inmutable/extensions/to/ToLocation.kt
@@ -2,60 +2,45 @@
  * © Copyright by Astride UG (haftungsbeschränkt) 2018 - 2019.
  */
 
+@file:Suppress("unused")
+
 package net.darkdevelopers.darkbedrock.darkness.spigot.location.location.inmutable.extensions.to
 
 import net.darkdevelopers.darkbedrock.darkness.spigot.location.location.inmutable.Location
 import net.darkdevelopers.darkbedrock.darkness.spigot.location.location.inmutable.LookableLocation
 import net.darkdevelopers.darkbedrock.darkness.spigot.location.location.inmutable.extensions.locationOf
 import net.darkdevelopers.darkbedrock.darkness.spigot.location.location.inmutable.extensions.lookableLocationOf
+import net.darkdevelopers.darkbedrock.darkness.spigot.location.location.inmutable.extensions.map
 import net.darkdevelopers.darkbedrock.darkness.spigot.location.lookable.inmutable.Lookable
 import net.darkdevelopers.darkbedrock.darkness.spigot.location.vector.inmutable.Vector3
-
-/*
- * @author Lars Artmann | LartyHD
- * Created by Lars Artmann | LartyHD on 25.05.2019 08:37.
- * Last edit 25.05.2019
- */
+import net.darkdevelopers.darkbedrock.darkness.spigot.location.vector.inmutable.extensions.alliases.Vector3D
+import net.darkdevelopers.darkbedrock.darkness.spigot.location.vector.inmutable.extensions.alliases.Vector3I
 
 /**
+ * Created on 25.05.2019 08:37.
  * @author Lars Artmann | LartyHD
- * Created by Lars Artmann | LartyHD on 25.05.2019 08:46.
- * Current Version: 1.0 (25.05.2019 - 25.05.2019)
  */
+
 fun <W, V : Vector3<*>> Pair<W, V>.toLocation(): Location<W, V> = locationOf(first, second)
 
-/**
- * @author Lars Artmann | LartyHD
- * Created by Lars Artmann | LartyHD on 25.05.2019 08:46.
- * Current Version: 1.0 (25.05.2019 - 25.05.2019)
- */
 fun <W, V : Vector3<*>, L : Lookable<*>> Triple<W, V, L>.toLookableLocation(): LookableLocation<W, V, L> =
     lookableLocationOf(first, second, third)
 
-/**
- * @author Lars Artmann | LartyHD
- * Created by Lars Artmann | LartyHD on 25.05.2019 08:55.
- * Current Version: 1.0 (25.05.2019 - 25.05.2019)
- */
 fun <W, V : Vector3<*>> V.toLocation(world: W): Location<W, V> = locationOf(world, this)
 
-/**
- * @author Lars Artmann | LartyHD
- * Created by Lars Artmann | LartyHD on 25.05.2019 08:56.
- * Current Version: 1.0 (25.05.2019 - 25.05.2019)
- */
 fun <W, V : Vector3<*>> Location<W, V>.toLocation(
     world: W = this.world,
     vector: V = this.vector
 ): Location<W, V> = locationOf(world, vector)
 
-/**
- * @author Lars Artmann | LartyHD
- * Created by Lars Artmann | LartyHD on 25.05.2019 08:59.
- * Current Version: 1.0 (25.05.2019 - 25.05.2019)
- */
 fun <W, V : Vector3<*>, L : Lookable<*>> LookableLocation<W, V, L>.toLookableLocation(
     world: W = this.world,
     vector: V = this.vector,
     lookable: L = this.lookable
 ): LookableLocation<W, V, L> = lookableLocationOf(world, vector, lookable)
+
+fun <W, OV : Vector3I> Location<W, OV>.toLocation3D(): Location<W, Vector3D> =
+    map(mappedVector = { this.map({ toDouble() }) })
+
+fun <W, OV : Vector3D> Location<W, OV>.toLocation3I(): Location<W, Vector3I> =
+    map(mappedVector = { this.map({ toInt() }) })
