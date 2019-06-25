2e12902d2f43b123126d1239491c45a75900c7b8
LartyHD <lartyhd@gmail.com>
2019-06-25 04:52:57 +0200

    Fixed alias "Location<*, Vector3<N>>.z"

--- Darkness/spigot/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/spigot/location/location/inmutable/extensions/alliases/Alliases.kt
+++ Darkness/spigot/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/spigot/location/location/inmutable/extensions/alliases/Alliases.kt
@@ -37,7 +37,7 @@ typealias DefaultLivingLocation = LivingLocation<Default>
 
 val <N> Location<*, Vector3<N>>.x: N get() = vector.x
 val <N> Location<*, Vector3<N>>.y: N get() = vector.y
-val <N> Location<*, Vector3<N>>.z: N get() = vector.y
+val <N> Location<*, Vector3<N>>.z: N get() = vector.z
 
 val <L> LookableLocation<*, *, Lookable<L>>.yaw: L get() = lookable.yaw
 val <L> LookableLocation<*, *, Lookable<L>>.pitch: L get() = lookable.pitch
