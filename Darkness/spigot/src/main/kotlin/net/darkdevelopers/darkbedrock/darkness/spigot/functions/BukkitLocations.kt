efdccefa3108f0a6c8f5f167f09bb9c1e501c943
LartyHD <lartyhd@gmail.com>
2019-06-27 00:03:47 +0200

    Added BukkitLocation.copy(...) #146

---
+++ Darkness/spigot/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/spigot/functions/BukkitLocations.kt
@@ -0,0 +1,32 @@
+/*
+ * © Copyright by Astride UG (haftungsbeschränkt) 2018 - 2019.
+ */
+
+@file:JvmName("BukkitLocationUtils")
+
+package net.darkdevelopers.darkbedrock.darkness.spigot.functions
+
+import net.darkdevelopers.darkbedrock.darkness.spigot.aliases.BukkitLocation
+import net.darkdevelopers.darkbedrock.darkness.spigot.aliases.BukkitWorld
+
+/*
+ * Created on 27.06.2019 00:01.
+ * @author Lars Artmann | LartyHD
+ */
+
+@JvmOverloads
+fun BukkitLocation.copy(
+    world: BukkitWorld? = null,
+    x: Double? = null,
+    y: Double? = null,
+    z: Double? = null,
+    yaw: Float? = null,
+    pitch: Float? = null
+): BukkitLocation = BukkitLocation(
+    world ?: this.world,
+    x ?: this.z,
+    y ?: this.y,
+    z ?: this.z,
+    yaw ?: this.yaw,
+    pitch ?: this.pitch
+)
