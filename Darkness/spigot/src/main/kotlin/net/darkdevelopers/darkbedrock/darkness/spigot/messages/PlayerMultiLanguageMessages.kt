3d28e7a2a1da5d9d6f271a0d70a54e7989b1645a
LartyHD <lartyhd@gmail.com>
2019-07-03 02:09:54 +0200

    Added a PlaceholderAPI is installed check in PlayerMultiLanguageMessages

--- Darkness/spigot/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/spigot/messages/PlayerMultiLanguageMessages.kt
+++ Darkness/spigot/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/spigot/messages/PlayerMultiLanguageMessages.kt
@@ -6,7 +6,9 @@ package net.darkdevelopers.darkbedrock.darkness.spigot.messages
 
 import me.clip.placeholderapi.PlaceholderAPI
 import net.darkdevelopers.darkbedrock.darkness.general.message.MultiLanguageMessages
+import org.bukkit.Bukkit
 import org.bukkit.entity.Player
+import org.bukkit.plugin.PluginManager
 import java.util.*
 
 /**
@@ -24,6 +26,7 @@ class PlayerMultiLanguageMessages : MultiLanguageMessages() {
         if (objects.isEmpty()) bundle.getString(string)
         else format(bundle, string.replaced(player), *objects)
 
-    private fun String.replaced(player: Player) = PlaceholderAPI.setPlaceholders(player, this)
+    private fun String.replaced(player: Player, pluginManager: PluginManager = Bukkit.getPluginManager()): String =
+        if (pluginManager.getPlugin("PlaceholderAPI") != null) PlaceholderAPI.setPlaceholders(player, this) else this
 
 }
