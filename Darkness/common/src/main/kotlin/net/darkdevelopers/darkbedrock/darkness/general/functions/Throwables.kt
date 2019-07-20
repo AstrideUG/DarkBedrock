3cc2224a11d404f001a94690be586c9fd870bbb6
LartyHD <lartyhd@gmail.com>
2019-07-21 01:54:28 +0200

    Added Throwable extensions Added a Throwable.crypt() to generate an unique string for every single Throwable

---
+++ Darkness/common/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/general/functions/Throwables.kt
@@ -0,0 +1,12 @@
+/*
+ * © Copyright by Astride UG (haftungsbeschränkt) 2018 - 2019.
+ */
+
+package net.darkdevelopers.darkbedrock.darkness.general.functions
+
+/**
+ * Created on 21.07.2019 01:46.
+ * @author Lars Artmann | LartyHD
+ */
+
+fun Throwable.crypt(): String = "${message?.sha256()}:${stackTrace.joinToString("\n").sha256()}"
