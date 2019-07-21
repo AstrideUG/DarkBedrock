fc2e87ce3501c178cdfe0bd33db42c9ba2f3f68d
LartyHD <lartyhd@gmail.com>
2019-07-21 02:49:49 +0200

    Added String.translateColors(Char) #161

---
+++ Darkness/spigot/src/main/kotlin/net/darkdevelopers/darkbedrock/darkness/spigot/functions/ChatColor.kt
@@ -0,0 +1,15 @@
+/*
+ * © Copyright by Astride UG (haftungsbeschränkt) 2018 - 2019.
+ */
+
+package net.darkdevelopers.darkbedrock.darkness.spigot.functions
+
+import org.bukkit.ChatColor
+
+/**
+ * Created on 21.07.2019 02:49.
+ * @author Lars Artmann | LartyHD
+ */
+
+fun String.translateColors(altColorChar: Char = '&'): String =
+    ChatColor.translateAlternateColorCodes(altColorChar, this)
